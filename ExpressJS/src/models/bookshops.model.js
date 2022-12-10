3a22001722d5bee227f331431d0258201da334d5
Fermin Alvarez <feeralvarez013@gmail.com>
2022-12-10 00:43:42 -0300

    Cambio en el formato de salida de endpoints de Entidad bookshops

--- ExpressJS/src/models/bookshops.model.js
+++ ExpressJS/src/models/bookshops.model.js
@@ -2,11 +2,11 @@ const database = require('../database/database');
 const table = "bookshops";
 
 async function findAll(){
-    return database.query(`SELECT * FROM ${table}`);
+    return database.query(`SELECT id_bookshop, name FROM ${table}`);
 }
 
 async function findAllBooks(ID){
-    return database.query(`SELECT * FROM bookshop_book WHERE id_bookshop = ${ID}`);
+    return database.query(`SELECT id_bookshop, name FROM bookshop_book WHERE id_bookshop = ${ID}`);
 }
 
 async function create(ID, name){
